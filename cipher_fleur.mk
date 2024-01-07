c4e31385ac6d3c36327512e97fb1c53cfbd02462
Ayan Roy <82808583+SimpleBoy99@users.noreply.github.com>
2024-01-07 15:34:06 +0530

    Update and rename aosp_fleur.mk to cipher_fleur.mk

--- cipher_fleur.mk
+++ cipher_fleur.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Aosp stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common cipher stuff
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -16,7 +16,7 @@ TARGET_SUPPORTS_TOUCHGESTURES := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := aosp_fleur
+PRODUCT_NAME := cipher_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1440
